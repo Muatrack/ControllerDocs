143ae8a868520be58ddf8f48347dc0eff6c0068f
Selo <fjfhccfkuk@outlook.com>
2024-12-07 14:40:29 +0800

    update

--- docs/run.sh
+++ docs/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-ps aux | grep 'sphinx-autobuild' | awk '{print $2}' | xargs kill -9
+ps aux | grep 'sphinx-autobuild' | grep '/usr/bin/python3' | awk '{print $2}' | xargs kill -9
 
+make clean
+make html
 sphinx-autobuild --host 0.0.0.0 source build/html &
